fd5b7d63c6d25e004d2be611cc8b957356233f97
Franklin Rodriguez <franklinserif@gmail.com>
2023-05-14 23:15:13 -0400

    fix type error

--- src/Context/entries/entiresContext.tsx
+++ src/Context/entries/entiresContext.tsx
@@ -4,7 +4,7 @@ import { Entry } from "@/interfaces";
 export interface ContextProps {
   entries: Entry[];
   addNewEntry: (description: string) => void;
-  updateEntry: (entry: Entry, showSnackbar: boolean) => void;
+  updateEntry: (entry: Entry, showSnackbar?: boolean) => void;
   deleteEntry: (id: string) => void;
 }
 
